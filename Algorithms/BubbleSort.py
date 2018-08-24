da6fa784dc3ab0d806284df8e33ac73884be65f4
bonevb <38665239+bonevb@users.noreply.github.com>
2018-08-24 12:34:02 +0300

    Update BubbleSort.py

--- Algorithms/BubbleSort.py
+++ Algorithms/BubbleSort.py
@@ -3,7 +3,7 @@ L = [4,6,5,1,3,2,7,11,9,10,8]
 def bubbleSort(L):
     for i in range(len(L)):
         j = i + 1
-        for j in range(i, len(L)-1):
+        for j in range(i, len(L)):
             if L[i] > L[j]:
                 L[i], L[j] = L[j], L[i]
 
